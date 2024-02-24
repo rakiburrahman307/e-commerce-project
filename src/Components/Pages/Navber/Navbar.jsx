ee19c2602b23b1184c210a95065fde59b9add5fd
rakiburrahman307 <rakiburrahman307@gmail.com>
2024-02-24 11:12:45 +0600

    set up project

---
+++ src/Components/Pages/Navber/Navbar.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Navbar.propTypes = {
+    
+};
+
+export default Navbar;
